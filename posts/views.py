35bd2f01c110eba68d37ce6646abd15941242901
Ingo Boernig <iboernig@boernig.loc>
2018-03-01 10:39:11 +0100

    create model and views posts

--- posts/views.py
+++ posts/views.py
@@ -7,4 +7,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('HELLO FROM POSTS')
+    return render(request, 'posts/index.html', { 'title': 'Latest Posts'})
+
+
